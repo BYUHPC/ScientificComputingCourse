43fcc4037b5d6dc8430620fb3bb5822baebf4c84
Levi Morrison <levi_morrison@byu.edu>
2016-12-29 12:09:16 -0700

    Rework Barrier to use completion function
    Also put it in FSL namespace

--- Threading/Barrier.hh
+++ Threading/Barrier.hh
@@ -1,26 +1,74 @@
-#ifndef BARRIER_HH
-#define BARRIER_HH
+#ifndef FSL_BARRIER_HH
+#define FSL_BARRIER_HH
 
 #include <mutex>
 #include <condition_variable>
 
+namespace fsl;
 
 class Barrier {
 
-    std::mutex m;
-    std::condition_variable cv;
-    unsigned int count;
-    unsigned int expect;
-    unsigned int generation;
+	std::mutex m;
+	std::condition_variable cv;
+	size_t count;
+	size_t expect;
+	size_t generation;
+	std::function<void()> cf;
 
 public:
 
-    explicit Barrier(unsigned int n);
+	// Barriers cannot be safely copied
+	Barrier(Barrier const &) = delete;
 
-    /**
-     * Returns true for the last thread in - false for all others
-     */ 
-    bool wait(); 
+	template<typename CompletionFunction>
+	explicit Barrier(size_t n, CompletionFunction f):
+		m{},
+		cv{},
+		count{n},
+		expect{n},
+		generation{0},
+		cf{f}
+	{}
+
+	void wait() {
+		// lock will be released at the end of scope
+		std::unique_lock<std::mutex> lock{m};
+
+		if (--count == 0) {
+			// this branch is taken by only the last thread
+			count = expect;
+
+			// increasing the generation will allow the
+			// waiting threads to continue
+			++generation;
+
+			// signal all the threads waiting on the
+			// condition to wake up
+			cv.notify_all();
+
+			// call the completion function
+			cf();
+
+		} else {
+
+			// capture the current generation
+			auto gen = generation;
+
+			// taken by all threads except the last
+			do {
+				// release lock and sleep until signalled
+				cv.wait(lock);
+				// upon waking the lock will be acquired
+
+			// While loop prevents spurious wake-ups.
+			// Also prevents issues with a thread coming
+			// back into the barrier before all the other
+			// threads have left.
+			} while (gen == generation);
+		}
+
+		// lock will be released upon returning
+	}
 
 };
 
