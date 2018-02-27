ba0b005e15f2c73f55324ae563ab69e4492c31c9
Levi Morrison <levim@php.net>
2018-02-27 13:40:09 -0700

    Format with clang-format

--- Threading/Barrier.hh
+++ Threading/Barrier.hh
@@ -1,90 +1,71 @@
 #ifndef FSL_BARRIER_HH
 #define FSL_BARRIER_HH
 
-#include <mutex>
 #include <condition_variable>
+#include <mutex>
 
 namespace fsl {
 
 class Barrier {
 
-	std::mutex m;
-	std::condition_variable cv;
-	size_t count;
-	size_t expect;
-	size_t generation;
-	std::function<void()> cf;
+  std::mutex m;
+  std::condition_variable cv;
+  size_t count;
+  size_t expect;
+  size_t generation;
+  std::function<void()> cf;
 
 public:
+  // Barriers cannot be safely copied
+  Barrier(Barrier const &) = delete;
+  Barrier &operator=(Barrier const &) = delete;
 
-	// Barriers cannot be safely copied
-	Barrier(Barrier const &) = delete;
-	Barrier & operator=(Barrier const &) = delete;
+  explicit Barrier(size_t n)
+      : m{}, cv{}, count{n}, expect{n}, generation{0}, cf{[]() {}} {}
 
-	explicit Barrier(size_t n):
-		m{},
-		cv{},
-		count{n},
-		expect{n},
-		generation{0},
-		cf{[]() {}}
-	{}
+  template <typename CompletionFunction>
+  Barrier(size_t n, CompletionFunction f)
+      : m{}, cv{}, count{n}, expect{n}, generation{0}, cf{f} {}
 
-	template<typename CompletionFunction>
-	Barrier(size_t n, CompletionFunction f):
-		m{},
-		cv{},
-		count{n},
-		expect{n},
-		generation{0},
-		cf{f}
-	{}
+  void wait() {
+    {
+      // lock will be released at the end of scope
+      std::unique_lock<std::mutex> lock{m};
 
-	void wait() {
-		{
-			// lock will be released at the end of scope
-			std::unique_lock<std::mutex> lock{m};
+      if (--count != 0) {
+        // taken by all threads except the last
 
-			if (--count != 0) {
-				// taken by all threads except the last
+        // capture the current generation
+        auto gen = generation;
 
-				// capture the current generation
-				auto gen = generation;
+        do {
+          // release lock and sleep until signalled
+          cv.wait(lock);
+          // upon waking the lock will be acquired
 
-				do {
-					// release lock and sleep until signalled
-					cv.wait(lock);
-					// upon waking the lock will be acquired
+          // While loop prevents spurious wake-ups.
+          // Also prevents issues with a thread coming back into the barrier
+          // before all the other threads have left.
+        } while (gen == generation);
 
-				// While loop prevents spurious wake-ups.
-				// Also prevents issues with a thread coming
-				// back into the barrier before all the
-				// other threads have left.
-				} while (gen == generation);
+        return; // lock is released
+      }
 
-				return; // lock is released
+      // remainder executed by only the last thread
 
-			}
+      cf();
+      count = expect;
 
-			// remainder executed by only the last thread
+      // Increasing the generation will allow the waiting threads to continue
+      // when they wake up.
+      ++generation;
 
-			cf();
-			count = expect;
+    } // lock is released
 
-			// increasing the generation will allow the
-			// waiting threads to continue when they wake
-			++generation;
-
-		} // lock is released
-
-		// signal all the threads waiting on the
-		// condition to wake up
-		cv.notify_all();
-
-	}
-
+    // signal all the threads waiting on the condition to wake up
+    cv.notify_all();
+  }
 };
-
 }
 
 #endif
--- Threading/WorkQueue.cc
+++ Threading/WorkQueue.cc
@@ -21,69 +21,64 @@ $ ./WorkQueue
 
 */
 
-class WorkQueue
-{
-    mutex m;
-    queue<int> q;
+class WorkQueue {
+  mutex m;
+  queue<int> q;
 
 public:
-    WorkQueue() = default;
-    ~WorkQueue() = default;
+  WorkQueue() = default;
+  ~WorkQueue() = default;
 
-    // bool indicates whether the queue is empty
-    // Only use the int if the bool is true
-    pair<bool, int> dequeue() {
-        unique_lock<mutex> lock(m);
-        if (q.empty()) {
-            return pair<bool, int>(true, 0);
-        }
-        else {
-            pair<bool, int> p{false, q.front()};
-            q.pop();
-            return p;
-        }
+  // bool indicates whether the queue is empty
+  // Only use the int if the bool is true
+  pair<bool, int> dequeue() {
+    unique_lock<mutex> lock(m);
+    if (q.empty()) {
+      return pair<bool, int>(true, 0);
+    } else {
+      pair<bool, int> p{false, q.front()};
+      q.pop();
+      return p;
     }
+  }
 
-    void enqueue(int item) {
-        unique_lock<mutex> lock(m);
-        q.push(item);
-    }
-
+  void enqueue(int item) {
+    unique_lock<mutex> lock(m);
+    q.push(item);
+  }
 };
 
-
-void worker(WorkQueue & queue, int tid, ostream & out) {
-    do {
-        pair<bool, int> r(queue.dequeue());
-        if (get<bool>(r)) {
-            return;
-        }
-        out << tid << ' ' << get<int>(r) << endl;
-    } while (true);
+void worker(WorkQueue &queue, int tid, ostream &out) {
+  do {
+    pair<bool, int> r(queue.dequeue());
+    if (get<bool>(r)) {
+      return;
+    }
+    out << tid << ' ' << get<int>(r) << endl;
+  } while (true);
 }
 
 int main(void) {
-    WorkQueue queue;
+  WorkQueue queue;
 
-    // produce all the work up front to simplify termination
-    for (int i = 0; i < 100000; ++i) {
-        queue.enqueue(i);
+  // produce all the work up front to simplify termination
+  for (int i = 0; i < 100000; ++i) {
+    queue.enqueue(i);
+  }
+
+  vector<thread> threads;
+  threads.emplace_back(worker, ref(queue), 0, ref(cout));
+  threads.emplace_back(worker, ref(queue), 1, ref(cerr));
+
+  int failed = 0;
+  for (auto &thread : threads) {
+    if (thread.joinable()) {
+      thread.join();
+    } else {
+      failed = 1;
+      cerr << "Thread was not joinable" << endl;
     }
+  }
 
-    vector<thread> threads;
-    threads.emplace_back(worker, ref(queue), 0, ref(cout));
-    threads.emplace_back(worker, ref(queue), 1, ref(cerr));
-
-    int failed = 0;
-    for (auto & thread : threads) {
-        if (thread.joinable()) {
-            thread.join();
-        } else {
-            failed = 1;
-            cerr << "Thread was not joinable" << endl;
-        }
-    }
-
-    return failed;
+  return failed;
 }
-
